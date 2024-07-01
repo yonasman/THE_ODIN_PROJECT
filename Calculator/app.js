cce8f7cbccf0ded95ce29bdcaee916b2173468e3
Yonas Negese M <yonasnegese250@gmail.com>
2024-07-02 01:48:21 +0300

    Add basic calculator functionality

--- Calculator/app.js
+++ Calculator/app.js
@@ -4,7 +4,6 @@
 // sum
 function sum(...args) {
     let answer = args.reduce((result,current) => result += current,0)
-    console.log(answer)
     return answer;
 }
 
@@ -58,37 +57,112 @@ function percent(num) {
 }
 
 // operate function
-let num1, num2, operator;
-// num1 = Number(prompt("Enter the first num"));
-// num2 = Number(prompt("Enter the second num"));
-// operator = prompt("Enter the operator")
 function operate(num1, num2, operator) {
     switch(operator) {
         case "+":
-            sum(num1, num2);
-            break;
+            return sum(num1, num2);
         case "-":
-            subtract(num1, num2);
-            break;
-        case "*":
-            multiply(num1, num2);
-            break;
+            return subtract(num1, num2);
+        case "x":
+            return multiply(num1, num2);
         case "/":
-            divide(num1, num2);
-            break;
+            return division(num1, num2);
         case "1/x":
-            inverse(num1);
-            break;
+            return inverse(num1);
         case "x^2":
-            square(num1, num2);
-            break;
+            return square(num1, num2);
         case "%":
-            percent(num1, num2);
-            break;
+            return percent(num1, num2);
         default:
-            console.log("Nothing to operate");
+            return "Nothing to operate";
     }
 }
-operate(num1,num2,operator);
-
-
+// reading the value of each button
+// ************************
+let num1 = "", num2 = "", operator = "";
+// displays
+let priDisplay = document.querySelector(".primary-display");
+let secDisplay = document.querySelector(".secondary-display");
+let buttons = document.querySelectorAll(".btn");
+let nowResult = 0
+buttons.forEach (
+    (button) => {
+        button.addEventListener("click", function(e) {
+            let value = e.target.value;
+            isFirstNum = true;
+            isSecNum = false;
+            if(value === "C") {
+                num1 = "";
+                num2 = "";
+                isFirstNum = true;
+                isSecNum = true;
+                operator = "";
+                nowResult = 0;
+                priDisplay.textContent = "";
+                secDisplay.textContent = "0";
+            } else if(value === "+" || value === "-" || value === "x" || value === "/") {
+                if(num1 !== "") {
+                    isFirstNum = false;
+                    operator = value;
+                    // console.log(num1);
+                    console.log("operator  " + operator)
+                    // priDisplay.textContent = operator;
+                    // secDisplay.textContent = operator;
+                    if(!nowResult) {
+                        priDisplay.textContent = `${num1} ${operator}`;
+                    } else {
+                        priDisplay.textContent = `${nowResult} ${operator}`;
+                    }
+                }
+            } else if(value === "BS") {
+                secDisplay.textContent = secDisplay.textContent.slice(0,-1);
+                num1 = secDisplay.textContent;
+                if(secDisplay.textContent === "") {
+                    secDisplay.textContent = 0;
+                    num1 = "";
+                }
+                console.log(secDisplay.textContent)
+            }
+            
+            
+            else if(num1 !== "" && operator !== "") {
+                //  && (value !== "+" || value !== "-" || value !== "x" || value !== "/")
+                    isFirstNum = false;
+                    isSecNum = true;
+                    console.log("number 2 is here")
+                    // ******
+                    // if(!isFirstNum && isSecNum) {
+                        num2 = value;
+                        console.log("num2  " + num2)
+                        secDisplay.textContent = num2;
+                    // }
+                    // let sum = 0;
+                    // if(!isFirstNum && isSecNum) {
+                        console.log(num1)
+                        console.log(num2)
+                        if(nowResult) {
+                            nowResult = operate(Number(nowResult),Number(num2),operator);
+                            console.log("current result inside " + nowResult)
+                            secDisplay.textContent = nowResult
+                            
+                        } else {
+                            nowResult = operate(Number(num1),Number(num2),operator);
+                            console.log("current result outside " + nowResult);
+                            secDisplay.textContent = nowResult
+                            // priDisplay.textContent = `${nowResult} ${operator}`
+                        }
+                       
+                    //    num1 = num2;
+                    // }
+                } 
+                else {    
+                    
+                if (isFirstNum) {
+                    num1 += value;
+                    console.log("num1  " + num1)
+                    secDisplay.textContent = num1;
+                } 
+                
+                }
+        })
+} )
